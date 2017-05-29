faec38242f75753841930948f9b619c363c493c1
Minako Nakanishi <n135216e@gmail.com>
2017-05-30 00:28:35 +0900

    ボタンを丸くした。送信ボタンへのマウスオーバー・マウスリーブイベント実装

---
+++ mock/main.js
@@ -0,0 +1,17 @@
+$(function() {
+  /** 送信ボタンにマウスオーバーした際のイベント */
+  $('.nikubtn').mouseover(function() {
+    $('button').css("background-color", "#c0fff4");
+
+    $('circle').css("fill", "#1e90ff").css("stroke", "#351fff");
+    $('ellipse').css("fill", "#1e90ff").css("stroke", "#351fff");
+
+  });
+  $('.nikubtn').mouseleave(function() {
+    /** 送信ボタンからマウスオーバーを外した際のイベント */
+    $('button').css("background-color", "#ffc0cb");
+
+    $('circle').css("fill", "#ffa500").css("stroke", "#a52a2a");
+    $('ellipse').css("fill", "#ffa500").css("stroke", "#a52a2a");
+  });
+});
