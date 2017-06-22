2c49b27fbf3e131443b8ba6a834a4c34d08392d3
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2017-06-23 01:07:23 +0900

    送信後のアニメーション。その１の動き実装

--- mock/main.js
+++ mock/main.js
@@ -13,11 +13,22 @@ document.addEventListener('DOMContentLoaded', function(e){
 
         // 1. contentが下に動く
         const contents = document.getElementById('contents');
+        // contents.addEventListener('animationend',() => {
+        //     this.classList.remove('');
+        // });
         contents.classList.add('contents_animation');
 
         // 2. textareaが上に動いて、メールの中へ入る
         const textarea = document.getElementById('textarea_wrap');
         textarea.classList.add('textarea_animation');
+
+        // 3. mail_topが閉じるアニメーション
+        const mail_top = document.getElementById('mail_top');
+        mail_top.classList.add('mail_top_animation');
+
+        // 4. 投稿ボタンを消す
+        const send_btn = document.getElementById('send_message_btn');
+        send_btn.style.visibility = 'hidden';
         
 
 
