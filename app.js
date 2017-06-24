beec27010058cee229ce04021f87371aabd3b14e
t-kurihara <takuya.kurihara00@gmail.com>
2017-06-24 15:18:20 +0900

    [bug fix]

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var contents = require('./routes/contents');
+var comment = require('./routes/comment');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/contents', contents);
+app.use('/comment', comment);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/comment.js
+++ routes/comment.js
@@ -1,22 +1,24 @@
+/* 投稿内容をDBに格納・更新する */
 'use strict';
 let express = require('express');
 let router = express.Router();
 let comment = require('./rakusjs_module/db/mongoCollectionComment');
 
-/*
-/ 投稿内容をDBに格納・更新する
-*/
+/* 投稿内容をDBにinsertする */
 router.post('/', function (req, res, next) {
 
     // insert
     try {
         comment().insertOne(req.query);
-        res.status(200);
+        res.status(200).json({
+            'status': true
+        });
 
     // error
     } catch (error) {
         res.status(500).json({
-            'error': error
+            'status': false
+            ,'error': error
         });
     }
 });
