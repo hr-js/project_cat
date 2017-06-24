a58c5d922a1e888bde5be116a2c9f751479dda98
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2017-06-24 17:05:56 +0900

    callバックアニメーション関数を作成

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,41 +3,41 @@
 // ノードが読み込まれたときに処理を実行する
 document.addEventListener('DOMContentLoaded', function(e){
 
-
-
-
-
     const btn = document.getElementById('send_message_btn');
-    btn.addEventListener('click', function(){
-        // メール送信アニメーション開始
+    btn.addEventListener('click', function(e){
 
-        // 1. contentが下に動く
-        const contents = document.getElementById('contents');
-        // contents.addEventListener('animationend',() => {
-        //     this.classList.remove('');
-        // });
-        contents.classList.add('contents_animation');
+        // 1. eventを止める
+        e.preventDefault();
 
-        // 2. textareaが上に動いて、メールの中へ入る
-        const textarea = document.getElementById('textarea_wrap');
-        textarea.classList.add('textarea_animation');
-
-        // 3. mail_topが閉じるアニメーション
-        const mail_top = document.getElementById('mail_top');
-        mail_top.classList.add('mail_top_animation');
-
-        // 4. 投稿ボタンを消す
-        const send_btn = document.getElementById('send_message_btn');
-        send_btn.style.visibility = 'hidden';
+        // 2. formを取得
+        const form = document.getElementsByTagName('form').form; 
         
+        // 3. メッセージを取得
+        const message = form.getElementsByTagName('textarea').input_message.value;
+
+        // 4. ajax通信
 
 
-        //     transform: scale(0.8) translateY(-200px);
-        // それに伴い中身は上に動く
-
-        // 元の位置にもどって、in
 
     });
 
-
 });
+
+// 通信成功時のコールバック
+function success(){
+    // 1. contentが下に動く
+    const contents = document.getElementById('contents');
+    contents.classList.add('contents_animation');
+
+    // 2. textareaが上に動いて、メールの中へ入る
+    const textarea = document.getElementById('textarea_wrap');
+    textarea.classList.add('textarea_animation');
+
+    // 3. mail_topが閉じるアニメーション
+    const mail_top = document.getElementById('mail_top');
+    mail_top.classList.add('mail_top_animation');
+
+    // 4. 投稿ボタンを消す
+    const send_btn = document.getElementById('send_message_btn');
+    send_btn.style.visibility = 'hidden';
+}
