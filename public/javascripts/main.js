b72c573e890948f3cfc023fbfd7e229ec94c6985
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2017-06-27 02:06:02 +0900

    アニメーションがループできるようにイベントを制御

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
           // success();
         } else {
           console.log('m9(^Д^)ﾌﾟｷﾞｬｰ：' + xhr.statusText);
+          success();
         }
       }
     };
@@ -77,4 +78,52 @@ function success() {
   // 7. 送信結果表示
   const result = document.getElementById('result');
   result.classList.add('show_result_animation');
+
+  // 8. アニメーションのコールバックを登録
+  result.addEventListener('animationend', resultCallback);
+
+  // 関数
+  function resultCallback(e){
+    console.log(`アニメーションの初期化`);
+    // formを隠す
+    mail_form.style.visibility = 'hidden';
+    // アニメーションで使用したクラスを初期化
+    contents.classList.remove('contents_animation');
+    textarea.classList.remove('textarea_animation');
+    mail_top.classList.remove('mail_top_animation');
+    send_btn.style.visibility = '';
+    cat_icon.classList.remove('cat_icon_animation');
+    mail_form.classList.remove('send_mail_animation');
+    result.classList.remove('show_result_animation');
+    // formを出す
+    const mail_back = document.getElementById('mail_back');
+    mail_back.style.visibility = 'hidden';
+    const mail_front = document.getElementById('mail_front');
+    mail_front.style.visibility = 'hidden';
+    mail_top.style.visibility = 'hidden';
+    mail_form.style.visibility = '';
+
+    // callbackを登録
+    textarea.addEventListener('animationend',textareaCallback);
+    // textarea出現アニメーション開始
+    textarea.classList.add('show_textarea_animation');
+
+    // イベントを解除
+    result.removeEventListener('animationend', resultCallback);
+  }
+
+  function textareaCallback(e){
+          //初期化
+      textarea.classList.remove('show_textarea_animation');
+      mail_back.style.visibility = '';
+      mail_front.style.visibility = '';
+      mail_top.style.visibility = '';
+      // focusイン
+      textarea.focus();
+
+      // イベントを解除
+      textarea.removeEventListener('animationend', textareaCallback);
+
+  }
+
 }
