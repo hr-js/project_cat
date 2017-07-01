aaaffa9ee90bf3735e015a67d575934eba17acf7
t-kurihara <takuya.kurihara00@gmail.com>
2017-07-01 21:49:02 +0900

    [add] server(today's post counter)

--- routes/comment.js
+++ routes/comment.js
@@ -9,6 +9,8 @@ router.post('/', function (req, res, next) {
    
     // insert
     try {
+        // toDate:findに影響無いのでコメント->検討中
+        // req.body.date = new Date(req.body.date);
         comment().insertOne(req.body);
         res.status(200).json({
             'status': true
@@ -21,6 +23,27 @@ router.post('/', function (req, res, next) {
             ,'error': error
         });
     }
+});
+
+
+router.get('/count', function (req, res, next) {
+
+    const TODAY = (new Date()).toString().replace(/T.*$/, '');
+    try{
+        comment().find({date:{"$gt":'2017-07-01'}}).toArray(function(err, docs){
+            res.status(200).json({
+                'status': true,
+                'count': docs.length
+            });
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            'status': false
+            ,'error': error
+        });
+    }
+
 });
 
 module.exports = router;
