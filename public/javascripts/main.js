6e789c466b44aed97221445552009571aa2a6574
tomoyuki-tanaka <masuminium5@gmail.com>
2017-07-01 10:24:50 +0900

    refactor: main.js and style.css

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -39,76 +39,43 @@ function postComment(data) {
 
 // 通信成功時のコールバック
 function success() {
-  // 1. contentが下に動く
-  const contents = document.getElementById('contents');
-  contents.classList.add('contents_animation');
+  const idNames = [
+    'contents', 'textarea_wrap', 'mail_top',
+    'send_message_btn', 'cat_icon', 'form',
+    'result', 'mail_back', 'mail_front'
+  ]
 
-  // 2. textareaが上に動いて、メールの中へ入る
-  const textarea = document.getElementById('textarea_wrap');
-  textarea.classList.add('textarea_animation');
+  const nodes = idNames.map(function(id) {
+    return [id, document.getElementById(id)]
+  })
 
-  // 3. mail_topが閉じるアニメーション
-  const mail_top = document.getElementById('mail_top');
-  mail_top.classList.add('mail_top_animation');
+  const target = new Map(nodes)
 
-  // 4. 投稿ボタンを消す
-  const send_btn = document.getElementById('send_message_btn');
-  send_btn.style.visibility = 'hidden';
-
-  // 5. おキャット様アイコンで手紙に封をする
-  const cat_icon = document.getElementById('cat_icon');
-  cat_icon.classList.add('cat_icon_animation');
-
-  // 6. 手紙を送る
-  const mail_form = document.getElementById('form');
-  mail_form.classList.add('send_mail_animation');
-
-  // 7. 送信結果表示
-  const result = document.getElementById('result');
-  result.classList.add('show_result_animation');
-
-  const mail_back = document.getElementById('mail_back');
-  const mail_front = document.getElementById('mail_front');
-
-  const target = {
-    contents: contents,
-    textarea: textarea,
-    mail_top: mail_top,
-    send_btn: send_btn,
-    cat_icon: cat_icon,
-    mail_form: mail_form,
-    result: result,
-    mail_back: mail_back,
-    mail_front: mail_front
-  };
+  // アニメーションの開始
+  target.forEach(function(node) {
+    node.classList.add('animation');
+  });
 
   result.addEventListener('animationend', resultCallbackCreate(target));
 }
 
 function animationInitialize(target) {
   // formを隠す
-  target.mail_form.style.visibility = 'hidden';
+  target.get('form').style.visibility = 'hidden';
 
-  // アニメーションで使用したクラスを初期化
-  target.contents.classList.remove('contents_animation');
-  target.textarea.classList.remove('textarea_animation');
-  target.mail_top.classList.remove('mail_top_animation');
-  target.cat_icon.classList.remove('cat_icon_animation');
-  target.mail_form.classList.remove('send_mail_animation');
-  target.result.classList.remove('show_result_animation');
-
-  // 送信ボタンを表示
-  target.send_btn.style.visibility = '';
+  target.forEach(function (node) {
+    node.classList.remove('animation');
+  });
 
   // 入力したメッセージ(あれな内容)を抹消
   document.getElementById('input_message').value = '';
 }
 
 function showMailForm (target) {
-  target.mail_back.style.visibility = 'hidden';
-  target.mail_front.style.visibility = 'hidden';
-  target.mail_top.style.visibility = 'hidden';
-  target.mail_form.style.visibility = '';
+  target.get('mail_back').style.visibility = 'hidden';
+  target.get('mail_front').style.visibility = 'hidden';
+  target.get('mail_top').style.visibility = 'hidden';
+  target.get('form').style.visibility = '';
 }
 
 function resultCallbackCreate(target) {
@@ -116,28 +83,30 @@ function resultCallbackCreate(target) {
     animationInitialize(target);
     showMailForm(target);
 
-    target.textarea.addEventListener('animationend',textareaCallbackCreate(target));
+    const textarea = target.get('textarea_wrap');
+    textarea.addEventListener('animationend',textareaCallbackCreate(target));
 
     // textarea出現アニメーション開始
-    target.textarea.classList.add('show_textarea_animation');
+    textarea.classList.add('show_textarea_animation');
 
-    target.result.removeEventListener('animationend', resultCallback);
+    target.get('result').removeEventListener('animationend', resultCallback);
   }
 }
 
 function textareaCallbackCreate(target) {
   // 初期化アニメーション後の処理
   return function textareaCallback(e) {
+    const textarea = target.get('textarea_wrap');
     //初期化
-    target.textarea.classList.remove('show_textarea_animation');
-    target.mail_back.style.visibility = '';
-    target.mail_front.style.visibility = '';
-    target.mail_top.style.visibility = '';
+    textarea.classList.remove('show_textarea_animation');
+    target.get('mail_back').style.visibility = '';
+    target.get('mail_front').style.visibility = '';
+    target.get('mail_top').style.visibility = '';
 
     // focusイン
-    target.textarea.focus();
+    textarea.focus();
 
     // イベントを解除
-    target.textarea.removeEventListener('animationend', textareaCallback);
+    textarea.removeEventListener('animationend', textareaCallback);
   }
 }
