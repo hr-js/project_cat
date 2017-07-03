38bd658b0742fed430eaa3c9f65b9f0036a7daf9
t-kurihara <takuya.kurihara00@gmail.com>
2017-07-03 22:54:29 +0900

    [uopdate] 当日の投稿件数 取得表示 | .gitignore

--- routes/comment.js
+++ routes/comment.js
@@ -1,12 +1,12 @@
-/* 投稿内容をDBに格納・更新する */
+/** 投稿内容をDBに格納・更新する */
 'use strict';
 let express = require('express');
 let router = express.Router();
 let comment = require('./rakusjs_module/db/mongoCollectionComment');
 
-/* 投稿内容をDBにinsertする */
+/** 投稿内容をDBにinsertする */
 router.post('/', function (req, res, next) {
-   
+    
     // insert
     try {
         // toDate:findに影響無いのでコメント->検討中
@@ -25,22 +25,22 @@ router.post('/', function (req, res, next) {
     }
 });
 
-
+/** 当日の投稿件数を取得 */
 router.get('/count', function (req, res, next) {
-
-    const TODAY = (new Date()).toString().replace(/T.*$/, '');
+    // 年月日のみ取得 
+    const TODAY = req.query.date.toString().replace(/T.*$/, '');
     try{
-        comment().find({date:{"$gt":'2017-07-01'}}).toArray(function(err, docs){
+        comment().find({date:{"$gt":TODAY}}).toArray(function(err, docs){
             res.status(200).json({
-                'status': true,
-                'count': docs.length
+                status: true,
+                count: docs.length
             });
         });
 
     } catch (error) {
         res.status(500).json({
-            'status': false
-            ,'error': error
+            status: false
+            ,error: error
         });
     }
 
