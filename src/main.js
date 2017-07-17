794e3efa8552b9ec3efdc57b0209d4038107ba2d
Tachikawa <k10d2019@gmail.com>
2017-07-17 10:25:35 +0900

    src/main.jsの本日の投稿件数修正

--- src/main.js
+++ src/main.js
@@ -113,11 +113,7 @@
       headers:myHeaders,
       body: JSON.stringify(data),
       mode: 'cors'
-    }).then(xhrErrorHandler).then(success).catch(function (err) {
-      return window.console.error(err);
-    }).fainally(function(){
-      getTodaysPostCount();
-    });
+    }).then(xhrErrorHandler).then(success).then(getTodaysPostCount).catch(console.error);
   }
 
   /**
